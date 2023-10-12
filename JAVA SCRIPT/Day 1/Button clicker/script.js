417f4ac52f5cba33b3d929ff34a62a12ea493d63
MANIOMARDEV <144290707+MANIOMARDEV@users.noreply.github.com>
2023-10-12 18:26:58 +0200

    first commit

---
+++ JAVA SCRIPT/Day 1/Button clicker/script.js
@@ -0,0 +1,21 @@
+// Function to log a message when the page is loaded
+console.log("page loaded...");
+
+// Function to toggle the text of a login button between "Login" and "Logout"
+function login(element) {
+    if (element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+// Function to display an alert when a like button is clicked
+function message() {
+    alert("Ninja was liked when clicking a like button");
+}
+
+// Function to hide an element from the page
+function hide(x) {
+    x.remove();
+}
